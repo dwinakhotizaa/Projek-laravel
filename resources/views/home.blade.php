f1733e0c0afc5cc8e29d844e9324ef83180656b7
unknown <dwinanurkhotiza20@gmail.com>
2024-06-13 19:53:17 +0700

    portfolio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/style.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <h2 class="logo">Porto<span style="color: #FB2576;">folio</span></h2>
+        <nav class="navigation">
+            <ul>
+                <li><a href="#Home">Home</a></li>
+                <li><a href="Project">Project </a></li>
+                <li><a href="#Certificate">Certificate</a></li>
+                <li><a href="/resume.pdf">
+                <button class="btn">Resume</button>
+                </a></li>
+            </ul>
+        </nav>
+    </header>
+
+     <!--HOME-->
+     <main>
+        <article>
+            <section class="home">
+                <div class="judul">
+                <img src="../image/f" alt="">
+                    <h2>HI, MY NAME IS <span style="color: red">DWINA NUR KHOTIZA</span></h2>
+                    <h4>I'M A STUDENT AT SMK WIKRAMA VOCATIONAL HIGH SCHOOL</h4>
+                    <p style="margin-left: 80px; ">Saat ini saya merupakan seorang pelajar yang duduk dibangku kelas 10 di SMK Wikrama Bogor. <br> Saya memiliki rasa keingintahuan dan mencoba hal baru yang besar mengenai bidan IT <br>oleh karena itu saya terjun pada Jurusan Pengembangan Perangkat Lunak dan Gim (PPLG)</p>
+                <div class="tombol">
+                    <button type="button">Download CV</button>
+                    <button type="button">View Project</button>
+                </div>
+             </section>
+        </article>
+
+         <!--Project-->
+    <article>
+        <section class="project" id="project">
+        <h1>MY PROJECT</h1>
+            <h3>Ini adalah beberapa project yang telah dikerjakan</h3>
+            <div class="parent-bx">
+                <div class="bx-one">
+                <img src="../image/PersonalWeb.jpeg" width="700px">
+                <div class="text-one">
+                <h4>Project Pertama</h4> 
+                <p>Personal Website</p>
+            </div>   
+            </div>
+
+            <div class="bx-two">
+                <img src="../image/WebShop.jpeg" width="700px">
+                <div class="text-two">
+                <h4>Project Kedua</h4> 
+                <p>Web Shop</p>
+            </div>  
+            </div>
+
+            <div class="bx-two">
+                <img src="../image/PerbandinganAngka.jpeg" width="700px">
+                <div class="text-two">
+                <h4>Project Ketiga (ATS)</h4> 
+                <p>Perbandingan Angka</p>
+            </div>  
+            </div>
+        </section>
+    </article>
+
+     <!--Certificate-->
+     <article>
+        <section class="certificate" id="certificate">
+            <div class="main">
+            <h1 id="f1">Certificate</h1>
+            <p>Berikut adalah sertifikat pembelajaran dasar web yang saya raih dari Dicoding Indonesia </p>
+                <img src="../image/SertifikatDicoding.jpeg" alt="">
+            <div class="certificate">
+            </div>
+            </div>
+        </section>
+    </article>
+
+     <!--FOOTER-->
+     <footer>
+            <div class="footer-contents">
+                <div>
+                    <h2 class="logo">Porto<span style="color: #FB2576;">folio</span></h2>
+                </div>
+                <p>Ada malam dan ada siang, ada bahagia ada kesedihan, ada matahari ada bulan, ada ujian ada kesabaran, itulah namanya kehidupan. Cukup sederhana jika kamu ingin bahagia, buatlah mudah tetapi berarti. </p>
+            </div>
+        
+            <div class="footer-contents">
+                <h4>Home</h4>
+                <li><a href="">About</a></li>
+                <li><a href="">Feature</a></li>
+                <li><a href="">Price</a></li>
+                <li><a href="">Contact</a></li>
+            </div>
+    
+            <div class="footer-contents">
+                <h4>Help</h4>
+                <li><a href="">Privacy policy</a></li>
+                <li><a href="">Terms of use</a></li>
+            </div>
+    
+            <div class="footer-contents">
+                <h4>Social</h4>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Twitter</a></li>
+                <li><a href="">Instagram</a></li>
+            </div>
+        </footer>
+     
+</body>
+</html>
